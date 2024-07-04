3b7c0fff21cbfbd5349fcad4b8813b9cfba99425
Asranov <asranov0003@gmail.com>
2024-07-04 10:26:49 +0500

    feat: install and add i18n react

---
+++ src/locales/i18n.ts
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import enTranslation from "./en/en.json";
+import ruTranslation from "./ru/ru.json";
+import uzTranslation from "./uz/uz.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: false,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      ru: {
+        translation: ruTranslation,
+      },
+      uz: {
+        translation: uzTranslation,
+      },
+    },
+  });
+
+export default i18n;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Home.css";
+import { useTranslation } from "react-i18next";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <h1>{t("labels.greeting")}</h1>
+    </>
+  );
 };
 
 export default Home;
